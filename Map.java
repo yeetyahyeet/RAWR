19b4de595ce5c33632ead6f4b2d4dad06239c3e5
Antioch Sanders <antiochsanders@gmail.com>
2015-07-21 23:05:19 -0400

    -Map.java and Sprite.java added

--- Map.java
+++ Map.java
@@ -1,2 +1,31 @@
+import java.awt.Graphics;
+
 public class Map {
+   private int myNumTilesWide, myNumTilesHigh,
+                    myTileWidth, myTileHeight;
+
+   public Map(int numTilesWide, int numTilesHigh, 
+                   int tileWidth, int tileHeight) {
+      myNumTilesWide = numTilesWide;
+      myNumTilesHigh = numTilesHigh;
+      myTileWidth = tileWidth;
+      myTileHeight = tileHeight;
+   }
+
+   public void draw(Graphics pen) {}
+
+   public void readData(String file) {}
+
+   public int getTileWidth() {
+      return myTileWidth;      
+   }
+   public int getTileHeight() {
+      return myTileHeight;
+   }
+   public int getNumTilesWide() {
+      return myNumTilesWide;
+   }
+   public int getNumTilesHigh() {
+      return myNumTilesHigh;
+   }
 }
